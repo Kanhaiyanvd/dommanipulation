8aada8af1a9596050c49e33bf405186c1e0b75c5
Kanhaiyanvd <118625121+Kanhaiyanvd@users.noreply.github.com>
2022-11-20 18:15:09 +0530

    add files via upload

---
+++ dommanipulation/task3.js
@@ -0,0 +1,7 @@
+var header = document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+
+
+ var header = document.getElementById('main');
+ header.style.fontWeight='bold';
+ header.style.color='green';
