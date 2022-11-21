cb9e92a7ab7bd79c09e543e830282f7ab6e4ab53
Kanhaiyanvd <118625121+Kanhaiyanvd@users.noreply.github.com>
2022-11-21 22:30:48 +0530

    queryselector and queryselectorall

---
+++ task6.js
@@ -0,0 +1,14 @@
+// let seconditem=document.querySelector('.list-group-item:nth-child(2)')
+  //  seconditem.style.backgroundColor='green'
+ //let  thirditem=document.querySelector('.list-group-item:nth-child(3)');
+   // thirditem.style.display='none';
+
+
+let seconditemfont = document.querySelectorAll('.list-group-item');
+  seconditemfont[1].style.color='green'
+
+
+let odd = document.querySelectorAll('li:nth-child(odd)')
+    for(var i=0;i<odd.length;i++){
+  odd[i].style.backgroundColor='green'
+    }
