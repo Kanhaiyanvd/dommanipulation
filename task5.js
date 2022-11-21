af79c6f59b1793cc2647c7853f70c0c07c079a74
Kanhaiyanvd <118625121+Kanhaiyanvd@users.noreply.github.com>
2022-11-21 22:13:17 +0530

    getelementbytagname

---
+++ task5.js
@@ -0,0 +1,5 @@
+//var lastitem=document.getElementsByClassName('lis')
+  // lastitem[0].style.fontWeight='bold'
+    
+    var li=document.getElementsByTagName('li');
+    li[4].style.backgroundColor='green'
